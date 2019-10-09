85715575c1e255243bd52f0f822063e6c4ec7635
Gary Liang <gary.liang@nab.com.au>
2019-10-09 11:34:11 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,69 @@
-import React, {useState} from "react";
-import RadioButton from './RadioButton';
-import styled from 'styled-components';
+import React from "react";
+import FileViewer from 'react-file-viewer';
+import { CustomErrorComponent } from 'custom-error';
+import fileUrl from './test_pdf.pdf';
 
-const TestDiv = styled.div`
-  border: ${props =>
-    props.disableBorder === true ? '0' : `solid 1px ${props => props.ownColor}`};
-`;
+/*
+FileViewer supported type
+----------------
+Images: png, jpeg, gif, bmp, including 360-degree images
+pdf
+csv
+xslx
+docx
+Video: mp4, webm
+Audio: mp3
 
-// https://codepen.io/satya164/pen/qdgYaO
+*/
+
+export function convertDataToBinary(base64Data) {
+  // win.atob === raw char
+  const raw = window.atob(base64Data);
+  // arr[unit_8][buffer]
+  const array = new Uint8Array(new ArrayBuffer(raw.length));
+
+  // arr[0]['a']
+  // arr[1]['b']
+  for (let i = 0; i < raw.length; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+
+  // re
+  return array;
+}
+
+// return blob
+export function convertDataToBlob(data, mimeType) {
+  // 1. data === base64
+  // 2. mineType === application/pdf
+  const arr = convertDataToBinary(data);
+  // blob([2d_arr], mime_type)
+  return new Blob([arr], {type: mimeType});
+}
+
 function App() {
-  const [value, setValue] = useState('');
+  //const fileUrl = 'http://www.pdf995.com/samples/pdf.pdf';
 
-  //test
-  console.log('value', value);
+  
+
+
+  //const file = require('./file.json');
+  const mimeType = 'pdf';
+  //const data = convertDataToBlob(file, mimeType);
+  //const tmpPath = window.URL.createObjectURL(data);
+
+  const onError = (e) => {
+    console.error(e);
+  }
 
   return <div className="App">
-    <TestDiv disableBorder={false} ownColor={'red'}>bla</TestDiv>
-
-    <RadioButton 
-      checked={value === 'test1' ? true : false}
-      onChange={() => {setValue('test1')}}
-      value={'test1'}
-      name={'test'}
-      label={'test radio1'}
-    ></RadioButton>
-    
-    <RadioButton 
-      checked={value === 'test2' ? true : false}
-      onChange={() => {setValue('test2')}}
-      value={'test2'}
-      name={'test'}
-      label={'test radio2'}
-    ></RadioButton>
+    <FileViewer
+      fileType={mimeType}
+      //filePath={tmpPath}
+      filePath={fileUrl}
+      errorComponent={CustomErrorComponent}
+      onError={onError}/>
+    );
   </div>;
 }
 
