7e91382428186ea4449d001bf077c854c54c8489
kenpeter <kenpeter@kenpeter.com>
2019-10-09 21:14:08 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React from 'react';
 import MaskedInput from 'react-text-mask';
-import createAutoCorrectedDatePipe from 'text-mask-addons/dist/createAutoCorrectedDatePipe'
+import createAutoCorrectedDatePipe
+  from 'text-mask-addons/dist/createAutoCorrectedDatePipe';
+import styled from 'styled-components';
 
-function App() {
-  
-  const autoCorrectedDatePipe = createAutoCorrectedDatePipe('dd/mm/yyyy');
-  return <div className="App">
-    
+const MyStyledInput = styled.input`
+  background: papayawhip;
+`;
 
-    <MaskedInput
-      //mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-      mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
-      placeholder={'DD/MM/YYYY'}
-      pipe={autoCorrectedDatePipe}
-      guide={true}
-      keepCharPositions={true}
-    />
-  </div>;
+function App () {
+  const autoCorrectedDatePipe = createAutoCorrectedDatePipe ('dd/mm/yyyy');
+  return (
+    <div className="App">
+
+      <MaskedInput
+        mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
+        placeholder={'DD/MM/YYYY'}
+        pipe={autoCorrectedDatePipe}
+        guide={true}
+        keepCharPositions={true}
+        render={(ref, props) => <MyStyledInput innerRef={ref} {...props} />}
+      />
+    </div>
+  );
 }
 
 export default App;
