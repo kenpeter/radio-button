fad928dd067623cb9d7ef87a6acaa733146354c2
Gary Liang <gary.liang@nab.com.au>
2019-10-09 15:20:10 +1100

    guide has to turn on

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 import MaskedInput from 'react-text-mask';
-import {createAutoCorrectedDatePipe} from 'text-mask-addons';
+import createAutoCorrectedDatePipe from 'text-mask-addons/dist/createAutoCorrectedDatePipe'
 
 function App() {
   
+  const autoCorrectedDatePipe = createAutoCorrectedDatePipe('dd/mm/yyyy');
   return <div className="App">
-    <MaskedInput
-      mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
-      //pipe={createAutoCorrectedDatePipe}
-      //keepCharPositions={true}
-    />
+    
 
     <MaskedInput
       //mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
       mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
-      className="form-control"
-      placeholder="DD/MM/YYYY"
-      pipe={createAutoCorrectedDatePipe()}
-      guide={false}
-      id="my-input-id"
-      onBlur={() => {}}
-      onChange={() => {}}
+      placeholder={'DD/MM/YYYY'}
+      pipe={autoCorrectedDatePipe}
+      guide={true}
+      keepCharPositions={true}
     />
   </div>;
 }
